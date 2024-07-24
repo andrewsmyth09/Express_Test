47dc6029f34bbbc49cd156890a81042098ef9eea
andrewsmyth09 <asmyth@gmx.com>
2024-07-24 14:58:21 +0200

    Update and delete requests added

--- routes/posts.js
+++ routes/posts.js
@@ -56,4 +56,30 @@ router.post('/', (req, res) => {
     res.status(201).json(posts);
 });
 
+// Update post
+router.put('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id = id);
+
+    if(!post) {
+        return res.status(404).json({msg: `No post was found with the id ${id}`});
+    };
+
+    post.title = req.body.title;
+    res.status(200).json(posts);
+});
+
+// Delete post
+router.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id = id);
+
+    if(!post) {
+        return res.status(404).json({msg: `No post was found with the id ${id}`});
+    };
+
+    posts = posts.filter((post) => post.id !== id);
+    res.status(200).json(posts);
+});
+
 export default router;
